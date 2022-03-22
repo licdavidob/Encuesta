bbac712665a3fbec45032ae57125b93719eaa007
Rszooc <cursoszudemy@gmail.com>
2022-03-22 09:32:03 -0600

    Agregado campo expediente

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,20 @@
 // Preguntas
 function miVal() {
-    const auxj    = document.full_form.juzgado.value;
-    const juzgado = parseInt(auxj.substring(0,2));
-    const parte   = parseInt(document.full_form.parte.value);
-    const in1     = parseInt(document.full_form.questionOne.value);
-    const in2     = parseInt(document.full_form.questionTwo.value);
-    const in3     = parseInt(document.full_form.questionThree.value);
-    const in4     = parseInt(document.full_form.questionFour.value);
-    const in5     = parseInt(document.full_form.questionFive.value);
-    const in6     = parseInt(document.full_form.questionSix.value);
-    const in7     = parseInt(document.full_form.questionSeven.value);
-    const in8     = document.full_form.comments.value; 
+    const auxj       = document.full_form.juzgado.value;
+    const expediente = document.full_form.expediente.value;
+    const juzgado    = parseInt(auxj.substring(0,2));
+    const parte      = parseInt(document.full_form.parte.value);
+    const in1        = parseInt(document.full_form.questionOne.value);
+    const in2        = parseInt(document.full_form.questionTwo.value);
+    const in3        = parseInt(document.full_form.questionThree.value);
+    const in4        = parseInt(document.full_form.questionFour.value);
+    const in5        = parseInt(document.full_form.questionFive.value);
+    const in6        = parseInt(document.full_form.questionSix.value);
+    const in7        = parseInt(document.full_form.questionSeven.value);
+    const in8        = document.full_form.comments.value; 
 
-    const respuestas = [juzgado, parte, in1, in2, in3, in4, in5, in6 ,in7, in8];
+    const respuestas = [juzgado, expediente, parte, in1, in2, in3, in4, in5, in6 ,in7, in8];
+    // console.log(respuestas);
     AgregarEncuesta(juzgado, parte, in1, in2, in3, in4, in5, in6 ,in7, in8);
 }
 function AgregarEncuesta(juzgado, parte, in1, in2, in3, in4, in5, in6 ,in7, in8) {
