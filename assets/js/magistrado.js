35587a60a272bb0ed6cf67e89f71cfd818420880
Rszooc <cursoszudemy@gmail.com>
2022-03-28 13:58:10 -0600

    No hagas lo que uri

--- assets/js/magistrado.js
+++ assets/js/magistrado.js
@@ -1,4 +1,4 @@
-const URL = "http://172.19.40.90/api/CRUD_Encuesta.php";
+const URLW = "http://172.19.40.90/api/CRUD_Encuesta.php";
 function Iniciar() {
   let Dia_Actual = moment().startOf("day").format("YYYY-MM-DD");
   let Fecha_Inicio = "2022-01-01";
@@ -12,7 +12,7 @@ function DataTable(Fecha_Inicio, Dia_Actual) {
       url: "//cdn.datatables.net/plug-ins/1.10.11/i18n/Spanish.json",
     },
     ajax: {
-      url: URL,
+      url: URLW,
       data: function (d) {
         d.Fecha_Inicio = Fecha_Inicio;
         d.Fecha_Fin = Dia_Actual;
@@ -139,7 +139,7 @@ function modalEncuesta(tbody, table) {
 
     $.ajax({
       data: parametros,
-      url: URL,
+      url: URLW,
       dataType: "json",
       type: "get",
       success: function (response) {
